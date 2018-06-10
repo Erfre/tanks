66019b2fd74c7fbc1d40726a33cbdc6225b07d14
erfre <erik.mfredriksson@gmail.com>
2018-06-10 16:02:06 +0200

    fixed autostart on camera.

--- controller.py
+++ controller.py
@@ -37,7 +37,12 @@ class controller:
         :return:
         """
         system("sudo pigpiod")
-        print("tank started")
+        system("uv4l -nopreview --auto-video_nr --driver raspicam --encoding
+        mjpeg --width 640 --height 480 --framerate 20 --hflip=yes --vflip=yes
+        --bitrate=2000000 --server-option '--port=8080' --server-option
+        '--max-queued-connections=30' --server-option '--max-streams=25'
+        --server-option '--max-threads=29'")
+        print("Livestream and pigpiod started.")
         self.hp = hp
         self.pi = pigpio.pi()
     
@@ -111,12 +116,3 @@ class controller:
                 pass
         
         self.servo_timer = time.time()
-
-    def fire(self, fire):
-        """
-
-        :param fire:
-        :return:
-        """
-        if fire:
-            pass  #  activate laser
