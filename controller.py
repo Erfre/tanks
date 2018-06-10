621d372d132a598fb8ee69c8562f8f7c2333c4e3
erfre <erik.mfredriksson@gmail.com>
2018-06-10 16:09:44 +0200

    fixed the multiline string

--- controller.py
+++ controller.py
@@ -37,11 +37,11 @@ class controller:
         :return:
         """
         system("sudo pigpiod")
-        system("uv4l -nopreview --auto-video_nr --driver raspicam --encoding
+        system("""uv4l -nopreview --auto-video_nr --driver raspicam --encoding
         mjpeg --width 640 --height 480 --framerate 20 --hflip=yes --vflip=yes
         --bitrate=2000000 --server-option '--port=8080' --server-option
         '--max-queued-connections=30' --server-option '--max-streams=25'
-        --server-option '--max-threads=29'")
+        --server-option '--max-threads=29'""")
         print("Livestream and pigpiod started.")
         self.hp = hp
         self.pi = pigpio.pi()
